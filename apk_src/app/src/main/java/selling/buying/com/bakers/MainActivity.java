725548255a451b5cd996554537fb1a4dbd6d477a
ROHIT POTTER <rohitpotter12@gmail.com>
2018-07-08 03:13:18 +0530

    uploading files

---
+++ apk_src/app/src/main/java/selling/buying/com/bakers/MainActivity.java
@@ -0,0 +1,13 @@
+package selling.buying.com.bakers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
